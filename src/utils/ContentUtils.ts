da32631e24e732247be28edbf39ce71663f33c36
griffinjoshs <leveloutusa@gmail.com>
2025-04-02 18:25:11 -0400

    url util

--- src/utils/ContentUtils.ts
+++ src/utils/ContentUtils.ts
@@ -12,6 +12,13 @@ export function normalizeRef(ref: any): string {
     }
     return '';
   }
+
+  export function normalizePath(path: string): string {
+    // If the path is just "/", leave it as is.
+    if (path === "/") return "/";
+    // Remove any trailing slashes
+    return path.replace(/\/+$/, "");
+  }  
   
   export function toArray(refOrArray: any): any[] {
     if (!refOrArray) return [];
