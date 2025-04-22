cb8fa09312d94f428512c75466607329f39c713b
griffinjoshs <leveloutusa@gmail.com>
2025-04-22 01:36:18 -0400

    services pages done

--- src/components/SectionVariants.js
+++ src/components/SectionVariants.js
@@ -11,7 +11,7 @@ import FAQimg from "@/assets/glen-rushton-PjeZGaKunAI-unsplash.jpg"
  */
 export const SectionVariants = {
 // ─────────── Benefits ───────────
-  benefits: {
+  imageCol: {
     sectionClass: "flex justify-center items-center text-left section-sm",
     contentClass:
       "flex justify-between items-start py-[var(--spacing-2xl)] flex-col-reverse md:flex-row-reverse gap-[var(--spacing-xl)] lg:gap-[var(--spacing-2xl)]",
@@ -25,6 +25,7 @@ export const SectionVariants = {
     itemPlacement: "top-content-section",
     childPlacement: "",
     childSlotClass: "lg:sticky lg:top-0",
+    buttonsPlacement: "top-content-section",
   },
 
   // ─────────── FAQ ───────────
